c950ff38d8fafb103e051a5653aa4e5ebe930534
Favour <ayofavour7@gmail.com>
2018-04-17 08:23:44 +0100

    Add files via upload

---
+++ checker.py
@@ -0,0 +1,20 @@
+def solution(N):
+    for num in range(1, N + 1):
+        if num%3 == 0:
+            print("Fizz")
+        elif num%5 == 0:
+            print("Buzz")
+        elif num%7 == 0:
+            print("Woof")
+        elif num%15 == 0:
+            print("FizzBuzz")
+        elif num%21 == 0:
+            print("FizzWoof")
+        elif num%105 == 0:
+            print("FizzBuzzWoof")
+        else:
+            print(num)
+
+solution(4)
+
+# print(solution(int(input("Range : "))))
